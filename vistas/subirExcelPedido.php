ab00cd7eeba80192e86c53eca7c7674abf5b71ff
SamucoPcAlmacen <almacen2@sanfernandosrl.com>
2019-10-21 16:28:49 -0400

    21/10/2019 prueba serv

--- vistas/subirExcelPedido.php
+++ vistas/subirExcelPedido.php
@@ -6,7 +6,7 @@ $target_path = "../libs/uploads/";
 $target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
     echo "El archivo ".  basename( $_FILES['uploadedfile']['name']). 
-    " ha sido subido";
+    " ha sido subido  <br>";
 } else{
     echo "Ha ocurrido un error, trate de nuevo!";
 }
@@ -33,6 +33,7 @@ if (($gestor = fopen($nombreArchivo, "r")) !== FALSE) {
     }
     fclose($gestor);
 }
+print_r($dato);
 
 foreach ($dato as $value) {
     try {
--- vistas/tables.php
+++ vistas/tables.php
@@ -4,14 +4,14 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
+          <h1 class="h3 mb-2 text-gray-800">Resumen de Pedidos Pendientes</h1>
+          <p class="mb-4">Resumen de pedidos pendientes de preparacion y/o Despacho, del mes en curso</a>.</p>
 
           <!-- DataTales Example -->
           
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+              <h6 class="m-0 font-weight-bold text-primary">Resumen Pedido</h6>
             </div>
             <div class="card-body">
               <div class="table-responsive">
