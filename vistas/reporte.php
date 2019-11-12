5bcc8e7da1d68f79489e88250d85f85ea82377bb
SamucoPcAlmacen <almacen2@sanfernandosrl.com>
2019-11-12 09:30:29 -0400

    reporte rapido

--- vistas/reporte.php
+++ vistas/reporte.php
@@ -1,95 +1,17 @@
-<table border="1" width="100%" cellspacing="0">
-    <tr colspan=14><h1><?php echo $_POST['reporte']; ?></h1></tr>
-    <tr>
-        <th>NoTran</th>
-        <th>Fecha</th>
-        <th>noFac</th>
-        <th>noClie</th>
-        <th>Cliente</th>
-        <th>CodVen</th>
-        <th>Picking</th>
-        <th>Revision</th>
-        <th>Embalaje</th>
-        <th>Falla</th>
-        <th>F.Preparacion</th>
-        <th>Cobrador</th>
-        <th>F.Salida</th>
-        <th>h.Salida</th>
+<?php 
+include '../includes/header.php'; 
+require '../config/conexion.php';
+$database = new Connection();
+$db = $database->openConnection();
 
-    </tr>
-    <?php
 
-    require '../config/conexion.php';
-    $database = new Connection();
-    $db = $database->openConnection();
+$resumen     =       $db->query("CALL resumenrapido({$_POST['nronota']})")->fetchAll();
 
-    if(isset($_POST['fechaInicio']) && isset($_POST['fechaFin'])){
-        $fechaInicial=$_POST['fechaInicio'];
-        $fechaFin=$_POST['fechaFin'];
-        $sql = 'CALL reporteFecha(:fechaInicial,:fechaFinal)';
-        // prepare for execution of the stored procedure
-        $stmt = $db->prepare($sql);
-        // pass value to the command
-        $stmt->bindParam(':fechaInicial', $fechaInicial);
-        $stmt->bindParam(':fechaFinal', $fechaFin);
-        // execute the stored procedure
-        $stmt->execute();
-        foreach ($stmt as $row) {
-        ?>
-        
-        <tr>
-        <td><?php echo $row['idNota']; ?></td>
-        <td><?php echo $row['fecha']; ?></td>
-        <td><?php echo $row['noFac']; ?></td>
-        <td><?php echo $row['noClie']; ?></td>
-        <td><?php echo $row['Cliente']; ?></td>
-        <td><?php echo $row['noVende']; ?></td>
-        <td><?php echo $row['piking']; ?></td>
-        <td><?php echo $row['revision']; ?></td>
-        <td><?php echo $row['embalaje']; ?></td>
-        <td><?php echo $row['falla']; ?></td>
-        <td><?php echo $row['fechaPreparacion']; ?></td>
-        <td><?php echo $row['cobrador']; ?></td>
-        <td><?php echo $row['fechaSalida']; ?></td>
-        <td><?php echo $row['horaSalida']; ?></td>
-     </tr>
-    
-        <?php } 
-        $database->closeConnection();
-      }elseif(isset($_POST['cod'])){  
-        $cod=$_POST['cod'];
-        //echo $cod;
-        $sql = 'CALL reporteCod(:cod)';
-        // prepare for execution of the stored procedure
-        $stmt = $db->prepare($sql);
-        // pass value to the command
-        $stmt->bindParam(':cod', $cod);
-        // execute the stored procedure
-        $stmt->execute();
-        foreach ($stmt as $row) {
-    ?>
+print_r($resumen );
+?>
 
-    <tr>
-        <td><?php echo $row['idNota']; ?></td>
-        <td><?php echo $row['fecha']; ?></td>
-        <td><?php echo $row['noFac']; ?></td>
-        <td><?php echo $row['noClie']; ?></td>
-        <td><?php echo $row['Cliente']; ?></td>
-        <td><?php echo $row['noVende']; ?></td>
-        <td><?php echo $row['piking']; ?></td>
-        <td><?php echo $row['revision']; ?></td>
-        <td><?php echo $row['embalaje']; ?></td>
-        <td><?php echo $row['falla']; ?></td>
-        <td><?php echo $row['fechaPreparacion']; ?></td>
-        <td><?php echo $row['cobrador']; ?></td>
-        <td><?php echo $row['fechaSalida']; ?></td>
-        <td><?php echo $row['horaSalida']; ?></td>
-    </tr>
-    <?php
-    }
-    $database->closeConnection();
-    }
-    ?>
-    </tbody>
-</table>
-              
+
+
+<?php 
+include '../includes/footer.php'; 
+?>
--- vistas/reporteDespacho.php
+++ vistas/reporteDespacho.php
@@ -1,51 +1,15 @@
-<?php include '../includes/header.php'; ?>
+<?php 
+include '../includes/header.php'; 
+?>
 
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+<form class="form-inline" method="POST" action="reporte.php">
+  <div class="form-group mx-sm-3 mb-2">
+    <label>Ingrese el Numero de Nota </label>
+    <input type="text" class="form-control" name="nronota" placeholder="# Nota">
+  </div>
+  <button type="submit" class="btn btn-primary mb-2">Buscar</button>
+</form>
 
-          <!-- Page Heading -->
-          <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
-
-          <!-- DataTales Example -->
-          
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Reporte Por Fecha</h6>
-            </div>
-            <div class="card-body col-md-3" >
-              <div class="form-group">
-                  <form action="reporte.php" method="post" target="_blank">
-                      <label for="">Fecha Inicio</label>
-                      <input type="date" class="form-control" name="fechaInicio">                    
-                      <label for="">Fecha Fin</label>
-                      <input type="date" class="form-control" name="fechaFin">
-                      <input type="hidden" value="Reporte por Fecha" name="reporte">
-                      <button class="btn btn-success" type="submit">Buscar</button>                    
-                  </form>
-              </div>
-            </div>
-          </div>
-          <div class="card shadow mb-4">
-            <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Reporte Por Codigo Nota</h6>
-            </div>
-            <div class="card-body col-md-3" >
-              <div class="form-group">
-                <form action="reporte.php" method="post" target="_blank">
-                    <label for="">Cod Nota</label>
-                    <input type="text" class="form-control" name="cod">    
-                    <input type="hidden" value="Reporte por Nota" name="reporte"> 
-                    <button class="btn btn-success" type="submit">Buscar</button>                    
-                </form>
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <!-- /.container-fluid -->
-
-      </div>
-      <!-- End of Main Content -->
-
-      <?php include '../includes/footer.php'; ?>   
+<?php 
+include '../includes/footer.php'; 
+?>
