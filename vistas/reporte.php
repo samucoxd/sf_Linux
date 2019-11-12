39e69cf57b5679e6b0657b33ea946481a52a4c08
SamucoPcAlmacen <almacen2@sanfernandosrl.com>
2019-11-12 09:43:09 -0400

    reporte rapido2

--- vistas/reporte.php
+++ vistas/reporte.php
@@ -4,14 +4,10 @@ require '../config/conexion.php';
 $database = new Connection();
 $db = $database->openConnection();
 
+echo $_POST['nronota'];
 $resumen = $db->query("CALL resumenrapido({$_POST['nronota']})")->fetchAll();
 
 echo count($resumen);
 
-?>
-
-
-
-<?php 
 include '../includes/footer.php'; 
 ?>
