ef3f4818b0ba83498a134469dfbf07fd2edda83e
SamucoPcAlmacen <almacen2@sanfernandosrl.com>
2019-11-15 11:29:05 -0400

    buscador por nota,fac,cliente,vendedor 1.3

--- vistas/reporteDespachoDetalle2.php
+++ vistas/reporteDespachoDetalle2.php
@@ -3,28 +3,23 @@ include '../includes/header.php';
 ?>
 <form action="reporteDespacho2.php" method="POST">
 <div>
-  <input type="radio" name="opcion" value="nota"
-         checked>
+  <input type="radio" name="opcion" value="nota"checked>
   <label for="nota">nota</label>
 </div>
 <div>
-  <input type="radio" name="opcion" value="factura"
-         >
+  <input type="radio" name="opcion" value="factura">
   <label for="factura">factura</label>
 </div>
 <div>
-  <input type="radio" name="opcion" value="cliente"
-         >
+  <input type="radio" name="opcion" value="cliente">
   <label for="cliente">cliente</label>
 </div>
 <div>
-  <input type="radio" name="opcion" value="vendedor"
-         >
+  <input type="radio" name="opcion" value="vendedor">
   <label for="vendedor">vendedor</label>
 </div>
 <div>
   <input type="text" name="valor" placeholder="Valor">
-         >
   <label for="Valor">Valor</label>
 </div>
     <button type="submit">Buscar</button>
