7244ff2167fc4860cfd9660884d1ee74460536b8
SamucoPcAlmacen <almacen2@sanfernandosrl.com>
2019-11-18 07:16:05 -0400

    subiendo al server los reporte 1.0

--- vistas/reporteDespachoDetalle2.php
+++ vistas/reporteDespachoDetalle2.php
@@ -1,25 +1,29 @@
 <?php 
 include '../includes/header.php'; 
 ?>
+<div class="container-fluid">
+  <div class="card shadow mb-4">
+    <div class="card-body">
+      <div class="table-responsive">
 <form action="reporteDespacho2.php" method="POST">
-<div>
-  <input type="radio" name="opcion" value="nota"checked>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="opcion" value="nota"checked>
   <label for="nota">nota</label>
 </div>
-<div>
-  <input type="radio" name="opcion" value="factura">
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="opcion" value="factura">
   <label for="factura">factura</label>
 </div>
-<div>
-  <input type="radio" name="opcion" value="cliente">
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="opcion" value="cliente">
   <label for="cliente">cliente</label>
 </div>
-<div>
-  <input type="radio" name="opcion" value="vendedor">
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="opcion" value="vendedor">
   <label for="vendedor">vendedor</label>
 </div>
-<div>
-  <input type="radio" name="opcion" value="cobrador">
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="opcion" value="cobrador">
   <label for="vendedor">Cobrador</label>
 </div>
 <div>
@@ -28,10 +32,14 @@ include '../includes/header.php';
 </div>
 <div>
   <input type="number" name="limite" value="10">
-  <label for="Valor">Valor</label>
+  <label for="Valor">Cantidad de Registros</label>
 </div>
     <button type="submit">Buscar</button>
 </form>
+      </div>
+      </div>
+      </div>
+      </div>
 <?php 
 include '../includes/footer.php'; 
 ?>
